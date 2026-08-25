aff6822abcbdf5809636ccb3b4d97bd8da41a76e
chr-d <275697199+chr-d@users.noreply.github.com>
2026-08-25 16:03:25 +0200

    add fix for missing leaflet marker icons in build

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,23 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
+import L from 'leaflet';
+import 'leaflet/dist/leaflet.css';
 import App from './App.tsx';
 import { AuthProvider } from './contexts';
 import './index.css';
+
+// --- LEAFLET ICON FIX ---
+import markerIconUrl from 'leaflet/dist/images/marker-icon.png';
+import markerIconRetinaUrl from 'leaflet/dist/images/marker-icon-2x.png';
+import markerShadowUrl from 'leaflet/dist/images/marker-shadow.png';
+
+delete (L.Icon.Default.prototype as any)._getIconUrl;
+L.Icon.Default.mergeOptions({
+  iconUrl: markerIconUrl,
+  iconRetinaUrl: markerIconRetinaUrl,
+  shadowUrl: markerShadowUrl
+});
+// ------------------------
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
